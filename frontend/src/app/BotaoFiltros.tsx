f465fbd7655b94017706468e54a35bc680afd354
Tiago Melo Juca <contact@tiagomelojuca.com>
2025-05-05 04:47:42 -0300

    ui improvs

--- frontend/src/app/BotaoFiltros.tsx
+++ frontend/src/app/BotaoFiltros.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import './BotaoFiltros.css'
 
 export default function BotaoFiltros({ handleClick }: { handleClick: () => void }) {
-    return <button className="btn-filtros" onClick={handleClick}>Filtros</button>
+    return <button className="btn-filtros" onClick={handleClick}>Filtrar</button>
 }
--- frontend/src/app/BotaoNovaInfracao.tsx
+++ frontend/src/app/BotaoNovaInfracao.tsx
@@ -3,5 +3,5 @@ import React from 'react'
 import './BotaoNovaInfracao.css'
 
 export default function BotaoNovaInfracao({ handleClick }: { handleClick: () => void }) {
-    return <button className="btn-nova-infracao" onClick={handleClick}>Nova Infração</button>
+    return <button className="btn-nova-infracao" onClick={handleClick}>Adicionar</button>
 }
